b0cab44089d860ee24294d496cfc3001bb793f7c
Mojalefa Motloung <mojalefa.motloung407@gmail.com>
2023-07-19 16:02:51 +0200

    Fixed content base model

--- edsa_recommender.py
+++ edsa_recommender.py
@@ -1,3 +1,119 @@
+# """
+
+#     Streamlit webserver-based Recommender Engine.
+
+#     Author: Explore Data Science Academy.
+
+#     Note:
+#     ---------------------------------------------------------------------
+#     Please follow the instructions provided within the README.md file
+#     located within the root of this repository for guidance on how to use
+#     this script correctly.
+
+#     NB: !! Do not remove/modify the code delimited by dashes !!
+
+#     This application is intended to be partly marked in an automated manner.
+#     Altering delimited code may result in a mark of 0.
+#     ---------------------------------------------------------------------
+
+#     Description: This file is used to launch a minimal streamlit web
+# 	application. You are expected to extend certain aspects of this script
+#     and its dependencies as part of your predict project.
+
+# 	For further help with the Streamlit framework, see:
+
+# 	https://docs.streamlit.io/en/latest/
+
+# """
+# # Streamlit dependencies
+# import streamlit as st
+
+# # Data handling dependencies
+# import pandas as pd
+# import numpy as np
+
+# # Custom Libraries
+# from utils.data_loader import load_movie_titles
+# from recommenders.collaborative_based import collab_model
+# from recommenders.content_based import content_model
+
+# # Data Loading
+# title_list = load_movie_titles('resources/data/movies.csv')
+
+# # App declaration
+# def main():
+
+#     # DO NOT REMOVE the 'Recommender System' option below, however,
+#     # you are welcome to add more options to enrich your app.
+#     page_options = ["Recommender System","Solution Overview"]
+
+#     # -------------------------------------------------------------------
+#     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
+#     # -------------------------------------------------------------------
+#     page_selection = st.sidebar.selectbox("Choose Option", page_options)
+#     if page_selection == "Recommender System":
+#         # Header contents
+#         st.write('# Movie Recommender Engine')
+#         st.write('### EXPLORE Data Science Academy Unsupervised Predict')
+#         st.image('resources/imgs/Image_header.png',use_column_width=True)
+#         # Recommender System algorithm selection
+#         sys = st.radio("Select an algorithm",
+#                        ('Content Based Filtering',
+#                         'Collaborative Based Filtering'))
+
+#         # User-based preferences
+#         st.write('### Enter Your Three Favorite Movies')
+#         movie_1 = st.selectbox('Fisrt Option',title_list)   # [14930:15200]
+#         movie_2 = st.selectbox('Second Option',title_list[1:]) #[25055:25255]
+#         movie_3 = st.selectbox('Third Option',title_list[2:]) #[21100:21200]
+#         fav_movies = [movie_1,movie_2,movie_3]
+
+#         # Perform top-10 movie recommendation generation
+#         if sys == 'Content Based Filtering':
+#             if st.button("Recommend"):
+#                 try:
+#                     with st.spinner('Crunching the numbers...'):
+#                         top_recommendations = content_model(movie_list=fav_movies,
+#                                                             top_n=10)
+#                     st.title("We think you'll like:")
+#                     for i,j in enumerate(top_recommendations):
+#                         st.subheader(str(i+1)+'. '+j)
+#                 except:
+#                     st.error("Oops! Looks like this algorithm does't work.\
+#                               We'll need to fix it!")
+
+
+#         if sys == 'Collaborative Based Filtering':
+#             if st.button("Recommend"):
+#                 try:
+#                     with st.spinner('Crunching the numbers...'):
+#                         top_recommendations = collab_model(movie_list=fav_movies,
+#                                                            top_n=10)
+                        
+#                     st.title("We think you'll like:")
+#                     for i,j in enumerate(top_recommendations):
+#                         st.subheader(str(i+1)+'. '+j)
+#                 except:
+#                     st.error("Oops! Looks like this algorithm does't work.\
+#                               We'll need to fix it!")
+
+
+#     # -------------------------------------------------------------------
+
+#     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+#     if page_selection == "Solution Overview":
+#         st.title("Solution Overview")
+#         st.write("Describe your winning approach on this page")
+
+#     # You may want to add more sections here for aspects such as an EDA,
+#     # or to provide your business pitch.
+
+
+# if __name__ == '__main__':
+#     main()
+
+
+
 """
 
     Streamlit webserver-based Recommender Engine.
@@ -32,20 +148,28 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+#Data Visualisation 
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+
 # Custom Libraries
 from utils.data_loader import load_movie_titles
 from recommenders.collaborative_based import collab_model
 from recommenders.content_based import content_model
-
+from recommenders.content_based import data_preprocessing
 # Data Loading
 title_list = load_movie_titles('resources/data/movies.csv')
-
+Ratings= pd.read_csv('resources/data/ratings.csv')
+# train= pd.read_csv('resources/data/train.csv')
+movies= pd.read_csv('resources/data/movies.csv')
 # App declaration
 def main():
 
     # DO NOT REMOVE the 'Recommender System' option below, however,
     # you are welcome to add more options to enrich your app.
-    page_options = ["Recommender System","Solution Overview"]
+    page_options = ["Recommender System","Solution Overview", "Insights", "About us"]
 
     # -------------------------------------------------------------------
     # ----------- !! THIS CODE MUST NOT BE ALTERED !! -------------------
@@ -63,7 +187,7 @@ def main():
 
         # User-based preferences
         st.write('### Enter Your Three Favorite Movies')
-        movie_1 = st.selectbox('Fisrt Option',title_list[14930:15200])
+        movie_1 = st.selectbox('First Option',title_list[14930:15200])
         movie_2 = st.selectbox('Second Option',title_list[25055:25255])
         movie_3 = st.selectbox('Third Option',title_list[21100:21200])
         fav_movies = [movie_1,movie_2,movie_3]
@@ -100,13 +224,79 @@ def main():
     # -------------------------------------------------------------------
 
     # ------------- SAFE FOR ALTERING/EXTENSION -------------------
+    
+    
     if page_selection == "Solution Overview":
         st.title("Solution Overview")
         st.write("Describe your winning approach on this page")
+        
+        
+        # User_Id = st.selectbox('User Id',title_list)
+        # Movie_Id = st.selectbox('Movie Id',title_list)
+        
 
     # You may want to add more sections here for aspects such as an EDA,
-    # or to provide your business pitch.
+    # or to provide your business pitch.'
+    if page_selection == "Insights":
+        st.title("Exploratory Data Analysis")
+        # st.image("resources\imgs\countplot.png",use_column_width=True)
+        st.image("resources\imgs\EDA.jpg",use_column_width=True)
+        
+        
+        mov= st.selectbox('Choose the movie you want to explore',title_list[1:15200])
+        
+        #Title of the graph
+        st.write(f'The distribution of {mov} ratings')
+        
+        #Combining two dataframes
+        Concat_df = pd.merge(movies, Ratings, on='movieId', how='inner')
+        
+        #Masking the selected movie:
+        Concat_df1=Concat_df[Concat_df['title'] == mov]
+        
+        if mov == "":
+            # Calculate the count of each rating
+            rating_counts = Ratings['rating'].value_counts().sort_index()
+        else:
+            rating_counts= Concat_df1['rating'].value_counts().sort_index()
 
+        # Create a bar graph using Plotly
+        fig = go.Figure(data=[go.Bar(x=rating_counts.index, y=rating_counts.values)])
+
+        # Customize the layout
+        fig.update_layout(
+        title="Users Movies Rating",
+        xaxis_title="Rating",
+        yaxis_title="Count"
+        )
+
+        # Render the figure in Streamlit using st.plotly_chart()
+        st.plotly_chart(fig)
+    
+        
+        st.header("Genre of the movie")
+        
+        movies['genres']=movies['genres'].str.replace("|", " ")
+        tabl=movies[movies['title']==mov]
+        text = ' '.join(tabl['genres'])
+        words = text.split()
+        for i in words:
+            st.write(i)
+            
+    if page_selection == "About us":
+        st.header("About us")
+        
+        st.title("Our Mission")
+        with st.expander('Click here to read more'):
+            st.write("Our mission is to harness the power of artificial intelligence to transform data into actionable insights, enabling businesses to make informed decisions and drive meaningful outcomes. We strive to develop cutting-edge predictive models and recommender systems that leverage advanced algorithms and data analytics techniques. By analyzing vast amounts of data, our aim is to empower organizations to unlock hidden patterns, optimize operations, and enhance customer experiences, ultimately driving innovation and growth")
+        
+        st.title("Our Team")
+        
+        st.image('resources/imgs/team.jpeg',use_column_width=True)
+        
+   
+        
 
 if __name__ == '__main__':
     main()
+
--- recommenders/content_based.py
+++ recommenders/content_based.py
@@ -35,9 +35,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Importing data
-movies = pd.read_csv('resources/data/movies.csv', sep = ',')
-ratings = pd.read_csv('resources/data/ratings.csv')
-movies.dropna(inplace=True)
+# movies = pd.read_csv('resources/data/movies.csv', sep = ',')
+# ratings = pd.read_csv('resources/data/ratings.csv')
+# movies.dropna(inplace=True)
+
+genome_scores = pd.read_csv('resources/data2/genome_scores.csv')
+genome_tags = pd.read_csv('resources/data2/genome_tags.csv')
+imdb_data = pd.read_csv('resources/data2/imdb_data.csv')
+links = pd.read_csv('resources/data2/links.csv')
+movies = pd.read_csv('resources/data2/movies.csv')
+tags = pd.read_csv('resources/data2/tags.csv')
+
 
 def data_preprocessing(subset_size):
     """Prepare data for use within Content filtering algorithm.
@@ -54,10 +62,19 @@ def data_preprocessing(subset_size):
 
     """
     # Split genre data into individual words.
-    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
-    # Subset of the data
-    movies_subset = movies[:subset_size]
-    return movies_subset
+    imdb_data['title_cast'] = imdb_data['title_cast'].str.replace('|', ' ')
+    imdb_data['plot_keywords'] = imdb_data['plot_keywords'].str.replace('|', ' ')
+    movies['genres'] = movies['genres'].str.replace('|', ' ')
+
+    temp = pd.merge(movies,imdb_data, on = 'movieId',how = 'left')
+    data = pd.merge(temp,links, on = 'movieId',how = 'left')
+
+    selected_columns = [ 'title', 'genres','title_cast','director','runtime','budget','plot_keywords','imdbId','tmdbId']
+    data['features'] = data[selected_columns].apply(lambda x: ' '.join(x.dropna().astype(str)), axis=1)
+    data = data[:subset_size]
+    return data
+
+
 
 # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
 # You are, however, encouraged to change its content.  
@@ -80,10 +97,12 @@ def content_model(movie_list,top_n=10):
     """
     # Initializing the empty list of recommended movies
     recommended_movies = []
-    data = data_preprocessing(27000)
+    data = data_preprocessing(17000)
     # Instantiating and generating the count matrix
     count_vec = CountVectorizer()
-    count_matrix = count_vec.fit_transform(data['keyWords'])
+    count_matrix = count_vec.fit_transform(data['features']) 
+
+    
     indices = pd.Series(data['title'])
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
     # Getting the index of the movie that matches the title
@@ -99,7 +118,9 @@ def content_model(movie_list,top_n=10):
     score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
     score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
     # Getting the indexes of the 10 most similar movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
+    
+    # listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
+    listings = pd.concat([score_series_1,score_series_2,score_series_3])
 
     # Store movie names
     recommended_movies = []
@@ -110,3 +131,154 @@ def content_model(movie_list,top_n=10):
     for i in top_indexes[:top_n]:
         recommended_movies.append(list(movies['title'])[i])
     return recommended_movies
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Script dependencies
+# import os
+# import pandas as pd
+# import numpy as np
+# from sklearn.metrics.pairwise import cosine_similarity
+# from sklearn.feature_extraction.text import CountVectorizer
+
+# # Importing data
+# movies = pd.read_csv('resources/data/movies.csv', sep = ',')
+# ratings = pd.read_csv('resources/data/ratings.csv')
+# movies.dropna(inplace=True)
+
+
+# def data_preprocessing(subset_size):
+#     """Prepare data for use within Content filtering algorithm.
+
+#     Parameters
+#     ----------
+#     subset_size : int
+#         Number of movies to use within the algorithm.
+
+#     Returns
+#     -------
+#     Pandas Dataframe
+#         Subset of movies selected for content-based filtering.
+
+#     """
+#     # Split genre data into individual words.
+#     movies['keyWords'] = movies['genres'].str.replace('|', ' ')
+#     # Subset of the data   Modified
+#     movies_subset = movies[:subset_size]
+#     return movies_subset
+
+
+
+# # !! DO NOT CHANGE THIS FUNCTION SIGNATURE !!
+# # You are, however, encouraged to change its content.  
+# def content_model(movie_list,top_n=10):
+#     """Performs Content filtering based upon a list of movies supplied
+#        by the app user.
+
+#     Parameters
+#     ----------
+#     movie_list : list (str)
+#         Favorite movies chosen by the app user.
+#     top_n : type
+#         Number of top recommendations to return to the user.
+
+#     Returns
+#     -------
+#     list (str)
+#         Titles of the top-n movie recommendations to the user.
+
+#     """
+#     # Initializing the empty list of recommended movies
+#     recommended_movies = []
+#     data = data_preprocessing(27000)
+#     # Instantiating and generating the count matrix
+#     count_vec = CountVectorizer()
+#     count_matrix = count_vec.fit_transform(data['keyWords']) 
+
+
+#     indices = pd.Series(data['title'])
+#     cosine_sim = cosine_similarity(count_matrix, count_matrix)
+#     # Getting the index of the movie that matches the title
+#     idx_1 = indices[indices == movie_list[0]].index[0]
+#     idx_2 = indices[indices == movie_list[1]].index[0]
+#     idx_3 = indices[indices == movie_list[2]].index[0]
+#     # Creating a Series with the similarity scores in descending order
+#     rank_1 = cosine_sim[idx_1]
+#     rank_2 = cosine_sim[idx_2]
+#     rank_3 = cosine_sim[idx_3]
+#     # Calculating the scores
+#     score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
+#     score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
+#     score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
+#     # Getting the indexes of the 10 most similar movies
+    
+#     # listings = score_series_1.append(score_series_2).append(score_series_3).sort_values(ascending = False)
+#     listings = pd.concat([score_series_1,score_series_2,score_series_3])
+
+#     # Store movie names
+#     recommended_movies = []
+#     # Appending the names of movies
+#     top_50_indexes = list(listings.iloc[1:50].index)
+#     # Removing chosen movies
+#     top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
+#     for i in top_indexes[:top_n]:
+#         recommended_movies.append(list(movies['title'])[i])
+#     return recommended_movies
